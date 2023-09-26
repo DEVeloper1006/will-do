07eebcac3527c2e8696bfa0d7dff0e7fbae8afd7
Dev Mody <devmody@devs-mbp-2.home>
2023-09-26 10:44:50 -0400

    Learned React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Alert, Breadcrumb, Card, Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Form.Group controlId='formEmail'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type='email' placeholder='Example@gmail.com' />
+            <Form.Text className='text-muted'>We will never share your address</Form.Text>
+          </Form.Group>
+          <Form.Group controlId='formPassword'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' placeholder='Password'/>
+          </Form.Group>
+          <Button variant='secondary' type='submit'>Login</Button>
+        </Form>
+        <Card className='mb-3' style={{ color: "#000"}}>
+          <Card.Img />
+          <Card.Body />
+          <Card.Title>Card Example</Card.Title>
+          <Card.Text>This is an example of react bootstrap cards</Card.Text>
+          <Button variant='primart'>Read More</Button>
+        </Card>
+        <Breadcrumb>
+          <Breadcrumb.Item>Test</Breadcrumb.Item>
+          <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+          <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+        </Breadcrumb> 
+        <Alert variant='primary'>This is a button</Alert>
+        <Button>Test Button</Button>
       </header>
     </div>
   );
